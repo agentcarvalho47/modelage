75ed57bf953694d2663a86d0afa8d6a79fc36829
agentcarvalho47 <carvalhoandre568@gmail.com>
2019-08-19 08:20:09 -0300

    Repository e Service para a classe Categoria

--- src/main/java/com/ifpi/ted2019/resources/CategoriaResource.java
+++ src/main/java/com/ifpi/ted2019/resources/CategoriaResource.java
@@ -1,23 +1,27 @@
 package com.ifpi.ted2019.resources;
 
 import java.util.ArrayList;
-import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import com.ifpi.ted2019.domain.*;
+import com.ifpi.ted2019.services.CategoriaService;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
-	@RequestMapping(method=RequestMethod.GET)
-	public List<Categoria> listar() {
-		List<Categoria> categorias = new ArrayList<Categoria>();
-		categorias.add(new Categoria(1, "Informática"));
-		categorias.add(new Categoria(2, "Escritório"));
-		categorias.add(new Categoria(3, "Laboratório"));
-		return categorias;
+	
+	@Autowired
+	private CategoriaService service;
+	
+	@RequestMapping(value="/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Categoria object = service.buscar(id);
+		return ResponseEntity.ok().body(object);
 	}
 	
 }
